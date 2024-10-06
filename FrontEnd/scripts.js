fdac07188ec6466f20248fc714cb8410392aa291
KJBarber89 <kjbarber89@gmail.com>
2024-10-06 13:01:27 -0500

    Add pop up for discounts

--- FrontEnd/scripts.js
+++ FrontEnd/scripts.js
@@ -6,3 +6,21 @@ document.addEventListener('DOMContentLoaded', () => {
         alert('Order submitted!');
     });
 });
+document.addEventListener('DOMContentLoaded', function() {
+    var popup = document.getElementById('popup');
+    var close = document.querySelector('.close');
+    var shopNowButton = document.querySelector('.popup-content button');
+    // Show the popup after 5 seconds
+    setTimeout(function() {
+        popup.style.display = 'flex';
+    }, 5000);
+
+    // Close the popup when the close button is clicked
+    close.addEventListener('click', function() {
+        popup.style.display = 'none';
+    });
+shopNowButton.addEventListener('click', function(){
+    popup.style.display = 'none';
+});
+
+});
